433377660179b3e3174a2ba4840115b857440230
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-09-26 01:55:25 +0530

    Create MainActivity.kt

---
+++ Flipkart/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication67
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Your logic here, for example, you can show the user’s email or name if you passed it via Intent
+    }
+}
