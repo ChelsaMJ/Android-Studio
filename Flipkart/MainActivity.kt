e23f5f9908b48d0dff9671d1baff2385b226b8ae
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-09-30 23:50:17 +0530

    Update MainActivity.kt

--- Flipkart/MainActivity.kt
+++ Flipkart/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.myapplication67
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Your logic here, for example, you can show the user’s email or name if you passed it via Intent
+        val welcome: TextView = findViewById(R.id.mj1)
+
+        welcome.setOnClickListener {
+            val wel = welcome.text.toString()
+
+            //explicit intent
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+        }
     }
 }
