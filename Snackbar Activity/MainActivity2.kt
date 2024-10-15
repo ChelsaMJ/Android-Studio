0144442d51af495ebaa4fdcb042e12faf68aab14
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-10-15 16:43:52 +0530

    Create MainActivity2.kt

---
+++ Snackbar Activity/MainActivity2.kt
@@ -0,0 +1,27 @@
+package com.example.snackbaractivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val welcome: TextView = findViewById(R.id.welcome1)
+
+        welcome.setOnClickListener {
+            val wel = welcome.text.toString()
+
+
+            //explicit intent
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
