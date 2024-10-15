71e07425f64b8c4b73d53574dd1ca7668461a320
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-10-15 16:42:39 +0530

    Create MainActivty1.kt

---
+++ Snackbar Activity/MainActivty1.kt
@@ -0,0 +1,30 @@
+package com.example.snackbaractivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main1)
+
+        val loginButton: Button = findViewById(R.id.button2)
+        val emailEditText: EditText = findViewById(R.id.editTextText2)
+        val passwordEditText: EditText = findViewById(R.id.editTextText3)
+
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            //explicit intent
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
