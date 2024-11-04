4ac196b5e75ab8b8a019d8b07ce59d2799db3212
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-11-04 13:18:11 +0530

    Create MainActivity1.kt

---
+++ Practice App/MainActivity1.kt
@@ -0,0 +1,21 @@
+package com.example.oct29
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_layout_page1)
+
+        val btn = findViewById<Button>(R.id.button3)
+        btn.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+}
