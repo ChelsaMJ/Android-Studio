990216e449f4dc3e4f0de6da09c0c11d4a23c9eb
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-11-28 15:29:30 +0530

    Add files via upload

---
+++ Password Theme/PwdTheme.kt
@@ -0,0 +1,17 @@
+package com.example.learningapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PwdTheme : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pwd_theme)
+
+
+    }
+}
